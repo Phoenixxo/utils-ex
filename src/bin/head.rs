9ee38eeaaf8fe37399e4229e22cc3d6ffa6aa6ab
Cayden Tisdale <caydentisdale43@gmail.com>
2026-05-03 05:25:45 -0400

    head implemented

--- src/bin/head.rs
+++ src/bin/head.rs
@@ -8,10 +8,12 @@ Author: [phoenixxo](https://github.com/phoenixxo)
 use std::{
     ffi::OsString,
     fs::File,
-    io::{BufReader, Write, stdout},
+    io::{BufReader, Read, Write, stdout},
     path::PathBuf,
     process::exit,
 };
+
+use memchr::memchr_iter;
 
 enum ArgKind {
     Lines,
@@ -22,17 +24,18 @@ pub fn main() {
     let args: Vec<OsString> = std::env::args_os().skip(1).collect();
 
     if args.is_empty() {
-        eprintln!("Usage: head [-n [lines] | -c [bytes]] <file1> <file2> ...".as_bytes());
+        eprintln!("Usage: head [-n [lines] | -c [bytes]] <file1> <file2> ...");
         exit(1);
     }
 
     let mut arg_kind: ArgKind = ArgKind::Lines; // Defaults to lines
     let mut output_size = 10; // Defaults to 10 lines
     let mut file_start_idx = 0; // First arg w/ file
+    let mut buffer = [0u8; 8192];
 
     if args[0] == "-n" || args[0] == "-c" {
         if args.len() < 3 {
-            eprintln!("Usage: head [-n [lines] | -c [bytes]] <file1> <file2> ...")
+            eprintln!("Usage: head [-n [lines] | -c [bytes]] <file1> <file2> ...");
             exit(1);
         }
 
@@ -50,13 +53,77 @@ pub fn main() {
 
     let files: Vec<PathBuf> = args[file_start_idx..].iter().map(PathBuf::from).collect();
 
-    for path in files {
+    for path in &files {
         let file = File::open(path).unwrap_or_else(|err| {
             eprintln!("Problem opening file: {:?}", err);
             exit(1);
         });
 
-        let reader = BufReader::new(file);
+        let mut reader = BufReader::new(file);
+        match arg_kind {
+            ArgKind::Bytes => {
+                let mut remaining_bytes: usize = output_size as usize;
+
+                loop {
+                    let n = reader
+                        .read(&mut buffer[..remaining_bytes.min(0x2000)])
+                        .unwrap_or_else(|err| {
+                            eprintln!("Error reading file: {:?}", err);
+                            exit(1);
+                        });
+
+                    if n == 0 {
+                        break;
+                    } // EOF
+
+                    if n >= remaining_bytes {
+                        stdout().write_all(&buffer[..remaining_bytes]).unwrap();
+                        break;
+                    }
+                    remaining_bytes -= n;
+                    stdout().write_all(&buffer[..n]).unwrap();
+                }
+            }
+            ArgKind::Lines => {
+                let mut lines_seen = 0;
+                loop {
+                    let n = reader.read(&mut buffer).unwrap_or_else(|err| {
+                        eprintln!("Error reading file: {:?}", err);
+                        exit(1);
+                    });
+
+                    if n == 0 {
+                        break;
+                    } // EOF
+
+                    let chunk = &buffer[..n];
+                    let mut prev = 0;
+
+                    for pos in memchr_iter(b'\n', &chunk) {
+                        lines_seen += 1;
+
+                        if lines_seen == output_size {
+                            match stdout().write_all(&chunk[prev..=pos]) {
+                                Ok(it) => it,
+                                Err(err) => return eprintln!("{:?}", err),
+                            };
+                            break;
+                        }
+
+                        prev = pos + 1;
+                    }
+
+                    if lines_seen == output_size {
+                        break;
+                    }
+
+                    match stdout().write_all(&chunk[prev..]) {
+                        Ok(it) => it,
+                        Err(err) => return eprintln!("{:?}", err),
+                    };
+                }
+            }
+        }
     }
 }
 
